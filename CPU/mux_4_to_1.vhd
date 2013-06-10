396b5e161b0e0d79aa387072a872d66bc8469559
sutong <1069700995@qq.com>
2013-06-10 22:15:23 +0800

    before change shl & shr B to A

---
+++ CPU/mux_4_to_1.vhd
@@ -0,0 +1,31 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux_4_to_1 is
+    port (input0, input1,
+          input2, input3 : in std_logic_vector(15 downto 0);
+          sel : in std_logic_vector(3 downto 0);
+          output : out std_logic_vector(15 downto 0));
+end mux_4_to_1;
+
+architecture behavioral of mux_4_to_1 is
+begin
+    mux : process(sel, input0, input1, input2, input3)
+    begin
+        case sel is
+            when "0000" =>
+                output <= input0;
+            when "0001" =>
+                output <= input1;
+            when "0010" =>
+                output <= input2;
+            when "0011" =>
+                output <= input3;
+			when others =>
+				output <= x"ffff";
+        end case;
+    end process;
+end behavioral;
+
+
+
