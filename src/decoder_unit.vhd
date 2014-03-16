edf40dac7a3f083d287822eb9ba577aa55145b40
Tong Su <reamandream@gmail.com>
2014-03-16 15:35:19 +0800

    add *.vhd files

---
+++ src/decoder_unit.vhd
@@ -0,0 +1,145 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use work.exp_cpu_components.all;
+
+entity decoder_unit is
+    port (IR : in std_logic_vector (15 downto 0);
+          SR : out std_logic_vector (3 downto 0);
+          DR : out std_logic_vector (3 downto 0);
+          op_code : out std_logic_vector (4 downto 0);
+          zj_instruct : out std_logic; -- jrz
+          cj_instruct : out std_logic; -- jrc
+          lj_instruct : out std_logic; -- long jmp, aka jmpa
+          rj_instruct : out std_logic; -- relative jmp, aka jr
+          nzj_instruct : out std_logic; -- jrnz
+          ncj_instruct : out std_logic; -- jrnc
+          DRWr : buffer std_logic;
+          mem_write : out std_logic;
+          dw_instruct : buffer std_logic;
+          change_z : out std_logic;
+          change_c : out std_logic;
+          sel_memdata : out std_logic;
+          r_sjmp_addr : out std_logic_vector (15 downto 0));
+end decoder_unit;
+
+architecture behavioral of decoder_unit is
+begin
+
+    sr <= ir (7 downto 4);
+    dr <= ir (3 downto 0);
+
+    sel_memdata <= ir (15) and
+                   (not ir (14)) and
+                   (not ir (13)) and
+                   (not ir (12));
+
+    change_z <= (not ir (15)) and
+                (not ir (14)) and
+                (not ir (13)) and
+                (not (ir (12) and
+                      ir (11) and
+                      ir (10) and
+                      ir (9) and
+                      ir (8)));
+    change_c <= (not ir (15)) and
+                (not ir (14)) and
+                (not ir (13)) and
+                ((not ir (0)) or
+                 ((not ir (12)) and
+                  ir (11) and
+                  ir (10)));
+
+    DRWr_proc : process (ir)
+    begin
+        if ir (15 downto 13) = "000" then -- 算术指令，包括mvrr
+            if ir (12) = '1' then -- modifiable
+                drwr <= '1';
+            else
+                drwr <= '0';
+            end if;
+        elsif ir (15 downto 12) = "1000" then
+            if ir (11 downto 8) = "0011" then -- strr
+                drwr <= '0';
+            else -- mvrd与ldrr
+                drwr <= '1';
+            end if;
+        else
+            drwr <= '0';
+        end if;
+    end process;
+
+    sjmp_addr_proc : process (ir)
+    begin
+        if ir (7) = '1' then
+            r_sjmp_addr <= "11111111" & ir (7 downto 0);
+        else
+            r_sjmp_addr <= "00000000" & ir (7 downto 0);
+        end if;
+    end process;
+
+    m_instruct : process (ir)
+    begin
+        if ir (15 downto 12) = "1000" then
+            case ir (11 downto 8) is
+                when "0001" => -- mvrd
+                    mem_write <= '0';
+                    dw_instruct <= '1';
+                when "0011" => -- strr
+                    mem_write <= '1';
+                    dw_instruct <= '0';
+                when others =>
+                    mem_write <= '0';
+                    dw_instruct <= '0';
+            end case;
+        elsif ir (15 downto 8) = "01001111" then -- jmpa
+            mem_write <= '0';
+            dw_instruct <= '1';
+		else
+            mem_write <= '0';
+            dw_instruct <= '0';
+        end if;
+
+    end process;
+
+    alu_op_code_proc : process (ir)
+    begin
+        if ir (15 downto 13) = "000" then
+            op_code <= ir (12 downto 8);
+        else
+            op_code <= "11111";
+        end if;
+    end process;
+
+    instruct_proc : process (ir)
+    begin
+		zj_instruct <= '0';
+		cj_instruct <= '0';
+		nzj_instruct <= '0';
+		ncj_instruct <= '0';
+		lj_instruct <= '0';
+		rj_instruct <= '0';
+        if ir (15 downto 12) = "0100" then
+
+            case ir (11 downto 8) is
+                when "0000" =>
+                    rj_instruct <= '1';
+                when "0100" =>
+                    cj_instruct <= '1';
+                when "0101" =>
+                    ncj_instruct <= '1';
+                when "0010" =>
+                    zj_instruct <= '1';
+                when "0011" =>
+                    nzj_instruct <= '1';
+                when "1111" =>
+                    lj_instruct <= '1';
+                when others =>
+                    null;
+            end case;
+        end if;
+    end process;
+end behavioral;
+
+
---
+++ src/exp_cpu.vhd
@@ -0,0 +1,168 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.exp_cpu_components.all;
+
+entity exp_cpu is port
+	(
+	clk : in std_logic;
+	reset : in std_logic;
+	WE : out std_logic;
+	AR : out std_logic_vector(15 downto 0);
+	OB : inout std_logic_vector(15 downto 0);
+    reg_sel : in std_logic_vector (5 downto 0);
+    reg_content : out std_logic_vector (15 downto 0);
+    c_flag : out std_logic;
+    z_flag : out std_logic
+	);
+end exp_cpu;
+
+architecture behav of exp_cpu is
+	
+signal t1, t2, t3 : std_logic;
+signal pc, pc_inc, IR : std_logic_vector(15 downto 0);
+	
+signal SR, DR : std_logic_vector(3 downto 0);
+signal op_code : std_logic_vector(4 downto 0);
+signal zj_instruct, cj_instruct, lj_instruct,
+       nzj_instruct, ncj_instruct, rj_instruct : std_logic;
+signal DRWr, Mem_Write, dw_instruct, change_z : std_logic;
+signal change_c, sel_memdata : std_logic;
+signal r_sjmp_addr : std_logic_vector(15 downto 0);
+	
+signal c_tmp, z_tmp, c_z_j_flag : std_logic;
+signal Mem_Addr, result, sjmp_addr : std_logic_vector(15 downto 0);
+	
+signal data_read, DR_data_out : std_logic_vector(15 downto 0);
+	
+signal r0, r1, r2, r3 : std_logic_vector (15 downto 0);
+signal output_DR, output_SR : std_logic_vector(15 downto 0);
+signal c_out, z_out : std_logic;  
+
+begin
+    test_out : process(reg_sel)
+    begin
+        case reg_sel is
+            when "000000" =>
+                reg_content <= r0;
+            when "000001" =>
+                reg_content <= r1;
+            when "000010" =>
+                reg_content <= r2;
+            when "000011" =>
+                reg_content <= r3;
+            when "000100" =>
+                reg_content <= "0000000" & t3 & "000" & t2 & "000" & t1;
+            when "111110" =>
+                reg_content <= pc;
+            when "111111" =>
+                reg_content <= ir;
+            when others =>
+                reg_content <= x"0000";
+        end case;
+    end process;
+
+G_INSTRU_FETCH : instru_fetch port map
+	   (reset => reset,
+		clk => clk,		
+		data_read => data_read,
+		lj_instruct => lj_instruct,
+		DW_instruct => DW_instruct,
+		c_z_j_flag => c_z_j_flag,
+		sjmp_addr => sjmp_addr,
+		t1 => t1,
+        t2 => t2,
+		t3 => t3,
+		pc => pc,
+		pc_inc => pc_inc,
+		IR => IR
+		); 
+
+G_DECODER : decoder_unit port map
+	   (IR => IR,
+		SR => SR,
+		DR => DR,
+		op_code => op_code,
+		zj_instruct => zj_instruct,
+		cj_instruct => cj_instruct,
+		lj_instruct => lj_instruct,
+        rj_instruct => rj_instruct,
+        nzj_instruct => nzj_instruct,
+        ncj_instruct => ncj_instruct,
+		DRWr => DRWr,
+		Mem_Write => Mem_Write,
+		dw_instruct => dw_instruct,
+		change_z => change_z,
+		change_c => change_c,
+		sel_memdata => sel_memdata,
+		r_sjmp_addr => r_sjmp_addr
+		);
+
+G_EXE : exe_unit port map
+		(
+	  	t1 => t1,
+		op_code => op_code,
+		zj_instruct => zj_instruct,
+	  	cj_instruct => cj_instruct,
+        nzj_instruct => nzj_instruct,
+        ncj_instruct => ncj_instruct,
+        rj_instruct => rj_instruct,
+	    pc => pc,
+		pc_inc => pc_inc,
+		c_in => c_out,
+	    z_in => z_out,
+		Mem_Write => Mem_Write,
+		c_tmp => c_tmp,
+		z_tmp => z_tmp,
+		c_z_j_flag => c_z_j_flag,
+		r_sjmp_addr => r_sjmp_addr,
+		dw_instruct => dw_instruct,
+		sjmp_addr =>  sjmp_addr,
+		SR_data => output_SR,
+		DR_data => output_DR,
+		Mem_Addr => Mem_Addr,
+		result => result
+	    );
+
+G_MEMORY : memory_unit port map
+	   (reset => reset,
+		clk   => clk,
+		t3    => t3, 
+		Mem_addr  => Mem_addr,  
+		Mem_Write => Mem_Write, 
+		sel_memdata => sel_memdata,  
+
+		SR_data => output_SR,
+		result => result, 
+		rw => WE,  
+		ob => ob,  
+		ar => AR,  
+		data_read   => data_read, 
+		DR_data_out => DR_data_out 
+		);
+
+G_REGFILE : regfile port map
+ 	   (DR => DR,  
+		SR => SR,  
+		reset => reset,
+		write => DRWr, 
+		clk => t3,	
+		d_input => DR_data_out, 
+		change_z => change_z, 
+		change_c => change_c, 
+	    c_in => c_tmp, 
+	    z_in => z_tmp, 
+
+		output_DR => output_DR,   
+		output_SR => output_SR,   
+		c_out => c_out,  
+		z_out => z_out,   
+		r0 => r0,
+		r1 => r1,
+		r2 => r2,
+		r3 => r3,
+		c_flag => c_flag,
+		z_flag => z_flag
+	    );
+
+end behav;
+
